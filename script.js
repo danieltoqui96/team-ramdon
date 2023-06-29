5edf3047c2214f2ae79e1d215a2f99702b9b85fe
Daniel Hernández <danieltoqui96@gmail.com>
2023-06-29 13:15:38 -0400

    arreglando casi todo

--- script.js
+++ script.js
@@ -1,60 +1,72 @@
-const generate = document.getElementById("generate");
-const clear = document.getElementById("clear");
+const randomButton = document.getElementById("random-button");
 const randomPoke = document.getElementsByClassName("random-poke")[0];
-const team = document.getElementById("team");
+const randomTeam = document.getElementsByClassName("team-random")[0];
 
-const teamRandom = [];
-
-const getPoke = async () => {
+randomButton.addEventListener("click", async () => {
   const apiUrl = `https://pokeapi.co/api/v2/pokemon/${
     Math.floor(Math.random() * 905) + 1
   }`;
-  // revisar
-  // if (teamRandom.length == 5) {
-  //   generate.style.visibility = "hidden";
-  // }
-  const spriteType = Math.random() < 0.9 ? "front_default" : "front_shiny";
+  const tipeSprite = Math.random() < 0.9;
+  const sprite = tipeSprite ? "front_default" : "front_shiny";
+
   try {
+    randomPokeDom(false);
+
     const response = await fetch(apiUrl);
     const data = await response.json();
     const pokemon = {
-      id: data.id,
       name: data.name,
-      sprite: data.sprites.other.home[spriteType],
-      isShiny: spriteType,
+      image: data.sprites.other.home[sprite],
+      isShiny: tipeSprite,
     };
     console.log(pokemon);
 
-    randomPoke.innerHTML = "";
-    const h2 = document.createElement("h2");
-    h2.textContent = pokemon.name;
-    const imgContainer = document.createElement("div");
-    imgContainer.classList.add("img-container");
-    const img = document.createElement("img");
-    img.src = pokemon.sprite;
-    img.alt = pokemon.name;
-    imgContainer.appendChild(img);
-    randomPoke.appendChild(h2);
-    randomPoke.appendChild(imgContainer);
+    randomPokeDom(true, pokemon);
+    pokeTeamDom(pokemon);
   } catch (error) {}
+});
+
+const randomPokeDom = (loading, pokemon = null) => {
+  randomPoke.innerHTML = "";
+
+  const h2 = document.createElement("h2");
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("img-container");
+  const img = document.createElement("img");
+
+  if (loading === true) {
+    h2.textContent = pokemon.name;
+    img.src = pokemon.image;
+    img.alt = pokemon.name;
+  } else {
+    h2.textContent = "Cargando";
+    img.src = "./img/ball.png";
+    img.alt = "ball";
+    img.classList.add("loader");
+  }
+  imgContainer.appendChild(img);
+  randomPoke.appendChild(h2);
+  randomPoke.appendChild(imgContainer);
 };
 
-function clearMain() {
-  randomPoke.innerHTML = `
-  <h2>Random</h2>
-  <img
-    class="sprite-main"
-    src="./img/ball.png"
-    alt="pokemon"
-  />`;
-  team.innerHTML = "";
-  for (let i = 0; teamRandom.length; i++) {
-    teamRandom.pop();
-  }
-  generate.style.visibility = "visible";
-  main.classList.remove("shiny");
-}
+const pokeTeamDom = (pokemon) => {
+  console.log("hola");
+  const card = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const span = document.createElement("span");
 
-// Agregamos un evento al botón para que al hacer clic se genere una nueva imagen
-generate.addEventListener("click", getPoke);
-clear.addEventListener("click", clearMain);
+  img.src = pokemon.image;
+  img.alt = pokemon.name;
+  span.textContent = pokemon.name;
+
+  card.classList.add("pokemon");
+  imgContainer.classList.add("img-pokemon");
+
+  imgContainer.appendChild(img);
+  card.appendChild(imgContainer);
+  card.appendChild(span);
+
+  randomTeam.appendChild(card);
+  console.log(randomTeam);
+};
